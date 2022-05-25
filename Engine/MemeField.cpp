d5c563b78b3ce75cc800423f03d31789a8a42eab
MrGam3z <68946439+MrGam3z@users.noreply.github.com>
2022-05-25 16:57:53 +0200

    Added a field border

--- Engine/MemeField.cpp
+++ Engine/MemeField.cpp
@@ -97,6 +97,7 @@ MemeField::MemeField(const Vei2& center, int nMemes)
 }
 
 void MemeField::Draw(Graphics& gfx) const {
+	gfx.DrawRect(GetRect().GetExpanded(borderThickness), borderColor);
 	gfx.DrawRect(GetRect(), SpriteCodex::baseColor);
 	for (Vei2 gridPos = { 0, 0 }; gridPos.y < height; gridPos.y++) {
 		for (gridPos.x = 0; gridPos.x < width; gridPos.x++) {
